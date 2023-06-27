6cce7e7a367709aa70b46c22d1572456707d17c6
Julian <julianser@hotmail.com.ar>
2023-06-27 16:04:03 -0300

    Se creo el modelo para la base de datos

---
+++ src/models/tarea.js
@@ -0,0 +1,16 @@
+import { Schema , model } from "mongoose";
+
+const tareaSchema = new Schema({
+    tarea:{
+        type: String,
+        minLength: 3,
+        maxLength: 100,
+        unique: true,
+        required: true,
+        match: "/^[A-Za-z0-9]{3,100}/"
+    }
+})
+
+const Tarea = model('tarea', tareaSchema);
+
+export default Tarea;
